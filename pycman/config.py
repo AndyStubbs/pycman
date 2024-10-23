1c0e5173d0e2307adbc1599aaf10804c514cb9fd
Andy <astubbs50@gmail.com>
2024-10-23 14:33:44 -0600

    Fixed encoding error and error formatting

--- pycman/config.py
+++ pycman/config.py
@@ -16,7 +16,7 @@ def load_config( config_type, config_path, prefix, postfix ):
 	
 	# A level file must be found
 	if len( files ) == 0:
-		raise FileNotFoundError( "No {config_type} files found." )
+		raise FileNotFoundError( f"No {config_type} files found." )
 	
 	# Sort the files to maintain order
 	files = sorted( files )
@@ -24,12 +24,12 @@ def load_config( config_type, config_path, prefix, postfix ):
 	# Read the files
 	for filename in files:
 		filepath = os.path.join( config_path, filename )
-		with open( filepath, "r" ) as file:
+		with open( filepath, "r", encoding="utf-8" ) as file:
 			content = file.read()
 		lines = content.splitlines()
 		if len( lines ) < 2:
 			raise Exception(
-				"Invalid format for {config_type} file {filename}. Must have more than 1 lines."
+				f"Invalid format for {config_type} file {filename}. Must have more than 1 lines."
 			)
 		level_name = lines[ 0 ]
 		if config_type == "charset":
